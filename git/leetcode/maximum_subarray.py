b32bd825cb628c954e7711ebea480e458b34dfe3
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-10-13 00:10:33 +0530

    .

---
+++ git/leetcode/maximum_subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+    def maxSubArray(self, nums):
+        # find out the subarray
+        # subarray always start with a positive number
+
+        # return sum of the found subarray
