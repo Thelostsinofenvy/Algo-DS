d88dba40b37d19063847063ce6df0a2764eff5cd
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-11-02 03:02:06 +0530

    finding first recurrinf character

---
+++ git/data structures/first_recurring.py
@@ -0,0 +1,26 @@
+array = [2, 5, 1, 2, 3, 5, 1, 2, 4]
+mylist = []
+# should return 2 as it is the first recurring character
+
+# brute force
+# On^2
+
+
+def recurring(array):
+    for i in range(0, len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] == array[j]:
+                return array[i]
+
+
+# better solution
+
+
+def alternative(array):
+    for i in range(0, len(array)):
+        if array[i] not in mylist:
+            mylist.append(array[i])
+    return mylist[0]
+
+
+print(alternative(array))
